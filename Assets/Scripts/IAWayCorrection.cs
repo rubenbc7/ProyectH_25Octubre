f1a6c18661601e48edcb02ddaad26634276ee459
Alberto Hoyos <53828141+Alberth-h@users.noreply.github.com>
2023-11-17 03:41:38 -0700

    IA MAMADA

---
+++ Assets/Scripts/IAWayCorrection.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+public class IAWayCorrection : MonoBehaviour
+{   [SerializeField] WaypointCircuit circuit;
+
+    int puntoActual = 0;
+
+    void Update()
+    {
+        Vector3 direccionCarro = transform.forward;
+        Vector3 direccionPuntoActual = circuit.Waypoints[puntoActual].position - transform.position;
+
+        float angulo = Vector3.Angle(direccionCarro, direccionPuntoActual);
+
+        if (angulo > 70f)
+        {
+            Rotar(direccionPuntoActual);
+        }
+        
+        if (Vector3.Distance(transform.position, circuit.Waypoints[puntoActual].position) < 20f)
+        {
+            puntoActual = (puntoActual + 1) % circuit.Waypoints.Length;
+        }
+    }
+
+    void Rotar(Vector3 direccionPuntoActual)
+    {
+        Quaternion rotacion = Quaternion.LookRotation(direccionPuntoActual);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotacion, Time.deltaTime);
+    }
+}
